e151059bd3d25a38d341630742140868f9e56580
Dariush Bahri <daz.b@xtec-ind.com>
2020-04-16 14:18:52 +0800

    Player layers now work. Working on more seemless looping using Timer instead of exit event

--- player.py
+++ player.py
@@ -16,34 +16,80 @@ from threading import Timer
 from omxplayer.player import OMXPlayer
 from tcp_server import PlayerTCPServer
 
+# Constants
+# Video player layers
+TOP_LAYER = 3
+MIDDLE_LAYER = 2
+BOTTOM_LAYER = 1
 
 class Player:
     """Main video player class"""
 
+    quitting = False
+
     def __init__(self):
         """Create an instance of the main video player application class"""
         self.video_path = "/home/pi/VideoPlayer/testfiles/bbb_sunflower_1080p_30fps_normal_Trim.mp4" # Test file
-        self.omxplayer = OMXPlayer(self.video_path, dbus_name='org.mpris.MediaPlayer2.omxplayer1', pause=True, args=['--no-osd', '--no-keys', '-b'])
-        self.player_end_timer = Timer(None, None)
+        # Init 2 OMX players
+        self._omxplayer_1 = OMXPlayer(self.video_path, dbus_name='org.mpris.MediaPlayer2.omxplayer1', pause=True, args=['--no-osd', '--no-keys', '-b'])
+        self._omxplayer_2 = OMXPlayer(self.video_path, dbus_name='org.mpris.MediaPlayer2.omxplayer2', pause=True, args=['--no-osd', '--no-keys', '-b'])
+        # Assign playeres to variables to make clear which ones are shown and hidden.
+        self.shown_player = self._omxplayer_1
+        self.hidden_player = self._omxplayer_2
+        # Set layer of players so we show the correct one
+        self.shown_player.set_layer(TOP_LAYER)
+        self.hidden_player.set_layer(BOTTOM_LAYER)
+        
+        self.loop = True
 
     def run(self):
         pass
 
     def play(self):
         print('Player sent play command')
-        self.player_end_timer = Timer(self.omxplayer.duration(), self.end_of_video)
-        self.player_end_timer.start()
-        self.omxplayer.play()
+        self.shown_player.play()
+        self.shown_player.exitEvent = self.end_of_video
 
-    def end_of_video(self):
+    def end_of_video(self, player, exit_status):
         print('Video ended')
-        self.omxplayer.load(self.video_path, pause=True)
-        self.play()
+        # Check we're not quitting the player
+        if not self.quitting:
+            self.switch_layers()
+
+            if self.loop:
+                self.play()
+            
+            self.load_new_player_hidden()
+            self.shown_player.set_layer(MIDDLE_LAYER)
+
+    def load_new_player_hidden(self):
+        if self.hidden_player == self._omxplayer_1:
+            self._omxplayer_1 = OMXPlayer(self.video_path, dbus_name='org.mpris.MediaPlayer2.omxplayer1', pause=True, args=['--no-osd', '--no-keys', '-b'])
+            self.hidden_player = self._omxplayer_1
+        else:
+            self._omxplayer_2 = OMXPlayer(self.video_path, dbus_name='org.mpris.MediaPlayer2.omxplayer2', pause=True, args=['--no-osd', '--no-keys', '-b'])
+            self.hidden_player = self._omxplayer_2
+        
+        self.hidden_player.set_layer(BOTTOM_LAYER)
+
+
+    def switch_layers(self):
+        """Switches players. Shown becomes Hidden and Hidden becomes Shown"""
+        if self.shown_player == self._omxplayer_1:
+            self.shown_player = self._omxplayer_2
+            self.hidden_player = self._omxplayer_1
+        else:
+            self.shown_player = self._omxplayer_1
+            self.hidden_player = self._omxplayer_2
+
+        # Show the new shown player
+        self.shown_player.set_layer(TOP_LAYER)
 
     def quit(self):
         """Shut down program"""
-        self.player_end_timer.cancel()
-        self.omxplayer.quit()
+        Player.quitting = True
+        self._omxplayer_1.quit()
+        self._omxplayer_2.quit()
         print('Player Shutdown')
 
 def cleanup(signum, frame):
@@ -56,8 +102,13 @@ def cleanup(signum, frame):
 # Main entry point.
 if __name__ == '__main__':
     print('And here we go....')
+
     LOG_FILENAME = 'debug.log'
-    logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
+    logging.basicConfig(
+        filename=LOG_FILENAME,
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.DEBUG,
+        datefmt='%Y-%m-%d %H:%M:%S')
 
     # Sets exit signals to call cleanup
     signal.signal(signal.SIGINT, cleanup)
