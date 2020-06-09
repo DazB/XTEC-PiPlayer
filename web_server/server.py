dd313fe7e697334d7c2c5aa9ace281998b0e6659
Dariush Bahri <daz.b@xtec-ind.com>
2020-06-09 07:58:03 +0100

    Got the network page beginning to work. Added some checking to the inputs to make sure they're kosher too.

--- web_server/server.py
+++ web_server/server.py
@@ -1,40 +1,112 @@
 from flask import Flask, render_template, request
 from werkzeug.serving import run_simple
 
+import re
 import configparser
+import os
 
 version = '0.1'
+config_path = '/home/pi/XTEC-PiPlayer/config.ini'
 
 app = Flask(__name__)
 
+# Homepage
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/index.html', methods=['POST', 'GET'])
 def index():
+    # If user has pressed "Save"
     if request.method == 'POST':
         try:
+            # Get the content and add to the ini file
             config = configparser.ConfigParser()
-            config.read('config.ini')
-            config['MP2']['DevDesc'] = request.form['DevDesc']
-            with open('config.ini', 'w') as configfile:
+            config.read(config_path)
+            config['MP2']['devdesc'] = request.form['devdesc']
+            with open(config_path, 'w') as configfile:
                 config.write(configfile)
         except Exception as ex:
-            return render_template('save_error.html', error="index.html: Error saving DevDesc to ini:" + str(ex))
+            print('index.html: Error saving devdesc' + str(ex))
+            return render_template('save_error.html', error="index.html: Error saving devdesc to ini:" + str(ex))
         
         return render_template('submit_ok.html')
+    # Else load page
     else:
-        dev_desc = ''
+        # Grab the details from the ini file to show on the page
+        dev_desc = '*error*'
         try:
             config = configparser.ConfigParser()
-            config.read('config.ini')
-            dev_desc = config['MP2']['DevDesc']
+            config.read(config_path)
+            dev_desc = config['MP2']['devdesc']
         except Exception as ex:
-            dev_desc = 'Error getting descirption'
+            print('index.html: Error getting info' + str(ex))
         return render_template('index.html', dev_desc=dev_desc, version=version)
 
+# Network settings page
+@app.route('/net.html', methods=['POST', 'GET'])
+def net():
+    # If user has pressed "Save"
+    if request.method == 'POST':
+        try:
+            # Check entered info is acceptable
+            ip = request.form['ip']
+            if not re.search(r'^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.'
+                r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.'
+                r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.'
+                r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', ip):
+                raise ValueError('Entered IP address of ' + ip + ' was incorrect')
+
+            port = request.form['port']
+            if not re.search(r'^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$', port):
+                raise ValueError('Entered Port number of ' + port + ' was incorrect')
+
+            subnet = request.form['subnet']
+            if not re.search(r'^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.'
+                r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.'
+                r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.'
+                r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', subnet):
+                raise ValueError('Entered Subnet mask of ' + subnet + ' was incorrect')
+
+        except Exception as ex:
+            # A value was incorrectly entered
+            return render_template('netdata_error.html', error=str(ex))
+        
+        # Values are okay. Save them to ini file
+        try:
+            # Get the content and add to the ini file
+            config = configparser.ConfigParser()
+            config.read(config_path)
+            config['MP2']['ip'] = ip
+            config['MP2']['port'] = port
+            config['MP2']['subnet'] = subnet
+            with open(config_path, 'w') as configfile:
+                config.write(configfile)
+        except Exception as ex:
+            return render_template('save_error.html', error="net.html: Error saving info to ini:" + str(ex))
+        
+        # Everything is hunky dory. Reboot the system
+        # os.system('sleep 3; sudo reboot')
+        return render_template('submit_ok.html')
+
+    # Else load page
+    else:
+        ip = '*error*'
+        port = '*error*'
+        subnet = '*error*'
+        try:
+            config = configparser.ConfigParser()
+            config.read(config_path)
+            ip = config['MP2']['ip']
+            port = config['MP2']['port']
+            subnet = config['MP2']['subnet']
+        except Exception as ex:
+            print('net.html: Error getting info' + str(ex))
+        return render_template('net.html', ip=ip, port=port, subnet=subnet)
+
+# Digital inputs page
 @app.route('/din.html')
 def din():
     return render_template('din.html')
 
+# Digital outputs page
 @app.route('/dout.html')
 def dout():
     return render_template('dout.html')
@@ -43,26 +115,24 @@ def dout():
 def gen_error():
     return render_template('gen_error.html')
 
-@app.route('/net.html')
-def net():
-    return render_template('net.html')
-
 @app.route('/netdata_error.html')
 def netdata_error():
     return render_template('netdata_error.html')
 
+# Others page
 @app.route('/oth.html')
 def oth():
     return render_template('oth.html')
 
+# Save error page. Shown when there is an error saving data
 @app.route('/save_error.html')
 def save_error():
     return render_template('save_error.html')
 
+# Submit successful page. Shown after successful save
 @app.route('/submit_ok.html')
 def submit_ok():
     return render_template('submit_ok.html')
-
 
 # def run_web_server(host):
 #     """Runs the webserver"""
@@ -71,3 +141,4 @@ def submit_ok():
 if __name__ == "__main__":
     """Runs the webserver"""
     app.run()
+    # app.run(host='192.168.1.105',port=8080)
