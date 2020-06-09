1afa49c180d383febd7d76ffb803c4628f3e2e98
Dariush Bahri <daz.b@xtec-ind.com>
2020-06-09 04:31:47 +0100

    Starting to get config changes in webpage working. Can save device description to config file

--- web_server/server.py
+++ web_server/server.py
@@ -1,12 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from werkzeug.serving import run_simple
+
+import configparser
+
+version = '0.1'
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index.html')
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/index.html', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        try:
+            config = configparser.ConfigParser()
+            config.read('config.ini')
+            config['MP2']['DevDesc'] = request.form['DevDesc']
+            with open('config.ini', 'w') as configfile:
+                config.write(configfile)
+        except Exception as ex:
+            return render_template('save_error.html', error="index.html: Error saving DevDesc to ini:" + str(ex))
+        
+        return render_template('submit_ok.html')
+    else:
+        dev_desc = ''
+        try:
+            config = configparser.ConfigParser()
+            config.read('config.ini')
+            dev_desc = config['MP2']['DevDesc']
+        except Exception as ex:
+            dev_desc = 'Error getting descirption'
+        return render_template('index.html', dev_desc=dev_desc, version=version)
 
 @app.route('/din.html')
 def din():
@@ -40,7 +63,11 @@ def save_error():
 def submit_ok():
     return render_template('submit_ok.html')
 
-def run_web_server(host):
-    """Runs the webserver"""
-    app.run(host=host, port=8080)
+
+# def run_web_server(host):
+#     """Runs the webserver"""
+#     app.run(host=host, port=8080, debug=True)
     
+if __name__ == "__main__":
+    """Runs the webserver"""
+    app.run()
