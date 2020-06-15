33664950b341fb43aa7c21c488457132986a5f07
Dariush Bahri <daz.b@xtec-ind.com>
2020-06-15 03:30:50 +0100

    Got input 1 and 2 correctly working, and able to change what happens on active and inactive

--- digital_io.py
+++ digital_io.py
@@ -4,6 +4,8 @@ import configparser
 import random
 from mp2_details import config_path
 
+# import debugpy
+
 class DigitalIO():
     """The digital IO control for the player"""
     
@@ -16,6 +18,9 @@ class DigitalIO():
         self.input_1.when_activated = self.input_1_active
         self.input_1.when_deactivated = self.input_1_inactive
 
+        self.input_2.when_activated = self.input_2_active
+        self.input_2.when_deactivated = self.input_2_inactive
+
     def do_action(self, action, track):
         """ Do player action depending on what is passed in"""
         if action == 'play':
@@ -23,24 +28,23 @@ class DigitalIO():
         elif action == 'loop':
             self.player.loop_command(track)
         elif action == 'random':
-            random_track = random.randint(1, track)
-            self.player.play_command(random_track)
+            random_track = random.randint(1, int(track))
+            self.player.play_command(str(random_track))
         elif action == 'stop':
-            self.player.stop_command()
+            self.player.stop_command('')
         elif action == 'pause':
-            self.player.pause_command()
+            self.player.pause_command('')
         elif action == 'audio_mute':
-            self.player.audio_mute_command(1)
+            self.player.audio_mute_command('1')
         elif action == 'video_mute':
-            self.player.video_mute_command(1)
+            self.player.video_mute_command('1')
         elif action == 'audio_unmute':
-            self.player.audio_mute_command(0)
+            self.player.audio_mute_command('0')
         elif action == 'video_unmute':
-            self.player.video_mute_command(0)
+            self.player.video_mute_command('0')
         
-
     def input_1_active(self):
-        # Read config file to see what action to take
+        print("Digital I/O: Input 1 active")
         config = configparser.ConfigParser()
         config.read(config_path)
         input_action = config['MP2']['input1_on']
@@ -49,4 +53,28 @@ class DigitalIO():
         self.do_action(input_action, input_track)
 
     def input_1_inactive(self):
-        pass
+        print("Digital I/O: Input 1 inactive")
+        config = configparser.ConfigParser()
+        config.read(config_path)
+        input_action = config['MP2']['input1_off']
+        input_track = config['MP2']['input1_off_track']
+
+        self.do_action(input_action, input_track)
+
+    def input_2_active(self):
+        print("Digital I/O: Input 2 active")
+        config = configparser.ConfigParser()
+        config.read(config_path)
+        input_action = config['MP2']['input2_on']
+        input_track = config['MP2']['input2_on_track']
+
+        self.do_action(input_action, input_track)
+
+    def input_2_inactive(self):
+        print("Digital I/O: Input 2 inactive")
+        config = configparser.ConfigParser()
+        config.read(config_path)
+        input_action = config['MP2']['input2_off']
+        input_track = config['MP2']['input2_off_track']
+
+        self.do_action(input_action, input_track)
