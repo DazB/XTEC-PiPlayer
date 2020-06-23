c5f113594a393e1731802970d097a364583efc29
Dariush Bahri <daz.b@xtec-ind.com>
2020-06-23 10:03:56 +0100

    Created USB Storage interface. Will now keep track if a USB stick is connected, and will search through it if a video cannot be found on main storage.

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ from player import Player
 from digital_io import DigitalIO
 from keyboard_control import KeyboardControl
 from ftp_server import XtecFTPServer
+import usb_storage
 
 class App:
     """Main Application class"""
@@ -329,6 +330,9 @@ class App:
         # Setup keyboard control
         self.keyboard_control = KeyboardControl(self.player)
 
+        # Start usb storage listener
+        self.usb_observer = usb_storage.start_listener()
+
         # Check auto start
         if auto_start == '1':
             self.player.play_command(auto_start_track)
@@ -338,6 +342,7 @@ class App:
         print('App: Cleaning up')
         self.player_tcp_server.server.quit()
         self.player.quit()
+        usb_storage.stop_listener(self.usb_observer)
         sys.exit('App: Quitting. Goodbye')
 
     def is_valid_ipv4(self, ip):
---
+++ usb_storage.py
@@ -0,0 +1,138 @@
+from pyudev import Context, Monitor, MonitorObserver
+import os
+import time
+
+usb_storage_path = 'usb_mount'
+
+# Globals for the device details
+USBDEV_UUID = None
+USBDEV_VENDOR = None
+USBDEV_SERID = None
+USBDEV_FSTYPE = None
+USBDEV_MODEL = None
+USBDEV_DEVPATH = None
+USBDEV_FILEPATH = None
+USBDEV_CONNECTED = False
+
+def usb_event_callback(action, device):
+    """Callback when a usb device is plugged in"""
+    global USBDEV_UUID
+    global USBDEV_VENDOR
+    global USBDEV_SERID
+    global USBDEV_FSTYPE
+    global USBDEV_MODEL
+    global USBDEV_DEVPATH
+    global USBDEV_FILEPATH
+    global USBDEV_CONNECTED
+
+    if action == 'add':
+        print('Usb storage: usb add')
+        # Store the device values
+        USBDEV_VENDOR = device.get('ID_VENDOR')
+        USBDEV_SERID = device.get('ID_SERIAL')
+        USBDEV_UUID = device.get('ID_FS_UUID')
+        USBDEV_FSTYPE = device.get('ID_FS_TYPE')
+        USBDEV_MODEL = device.get('ID_MODEL')
+        USBDEV_DEVPATH = device.get('DEVNAME')
+        USBDEV_CONNECTED = True
+
+        # Check if the dev path exists
+        if os.path.exists(USBDEV_DEVPATH):
+
+            # Create a mount directory
+            if not os.path.exists(usb_storage_path):
+                os.makedirs(usb_storage_path)
+
+            try:
+                # Mount the dev path to the folder
+                os.system("sudo mount " + USBDEV_DEVPATH + " " + usb_storage_path)
+                # Return the path to the folder from root
+                USBDEV_FILEPATH = os.getcwd() + '/' + usb_storage_path
+
+            except Exception as ex:
+                print('Usb storage: Error mounting usb: ' + str(ex))
+            
+
+    elif action == 'remove':
+        print('Usb storage: usb remove')
+        if USBDEV_CONNECTED:    
+            # Clear the device data
+            USBDEV_VENDOR = None
+            USBDEV_SERID = None
+            USBDEV_UUID = None
+            USBDEV_FSTYPE = None
+            USBDEV_MODEL = None
+            USBDEV_DEVPATH = None
+            USBDEV_CONNECTED = False
+
+            try:
+                # Unmount the dev path to the folder
+                os.system("sudo umount " + USBDEV_FILEPATH)
+                os.system("sudo mount " + USBDEV_DEVPATH + " " + usb_storage_path)
+                USBDEV_FILEPATH = None
+
+            except Exception as ex:
+                print('Usb storage: Error unmounting usb: ' + str(ex))
+
+
+def start_listener():
+    """Create a context, create monitor at kernel level, select devices"""
+    context = Context()
+    monitor = Monitor.from_netlink(context)
+    monitor.filter_by(subsystem='block')
+
+    observer = MonitorObserver(monitor, usb_event_callback, name="usbdev")
+    observer.start()
+    return observer
+
+def get_device_data():
+    if USBDEV_FILEPATH != None:
+        global USBDEV_UUID
+        global USBDEV_VENDOR
+        global USBDEV_SERID
+        global USBDEV_FSTYPE
+        global USBDEV_MODEL
+        global USBDEV_DEVPATH
+        return {'UUID': USBDEV_UUID,
+               'SERID': USBDEV_SERID, 
+               'VENDOR': USBDEV_VENDOR, 
+               'FSTYPE': USBDEV_FSTYPE,
+               'MODEL': USBDEV_MODEL,
+               'DEVPATH': USBDEV_DEVPATH}
+    return None
+
+def stop_listener(observer):
+    observer.stop()
+
+#start listening for usb device change events
+#you may have to unplug the flashdrive and replug
+# start_listener()
+
+# while 1:
+#     time.sleep(1)
+
+#     #get some identification data 
+#     #returns a dict with keys UUID, SERID (for serial ID), 
+#     #VENDOR (the manufacturer), FSTYPE (file system), MODEL (the model), DEVPATH for the path under ~/dev.
+#     device = get_device_data()
+
+#     #get the path (currently set for Rpi, can be changed)
+#     # path = get_mount_path_usb_device()
+
+#     print("dev: " + str(device))
+#     # print("path: " + str(path))
+#     print("---------------------------------")
+
+
+# context = Context()
+# monitor = Monitor.from_netlink(context)
+# monitor.filter_by(subsystem='block')
+# def print_device_event(device):
+#     print('background event {0.action}: {0.device_path}'.format(device))
+# observer = MonitorObserver(monitor, callback=print_device_event, name='monitor-observer')
+# observer.daemon
+# observer.start()
+
+
+# while 1:
+#     time.sleep(10)
